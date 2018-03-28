abe62395f3efcdb81163f7a62c2fb40279e0fe4e
eyalmnm <eyal@em-projects.com>
2018-03-28 13:30:45 +0300

    Initial commit

---
+++ Utils/JsonUtils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallerAppServer.Utils
+{
+    public class JsonUtils
+    {
+        public static String toJsonStr(Dictionary<String, object> data)
+        {
+            String jsonString = "{";
+            foreach (KeyValuePair<String, object> item in data)
+            {
+                jsonString += '\"' + item.Key + '\"';
+                jsonString += ':';
+                if (item.Value is string)
+                {
+                    String value = (string)item.Value;
+                    if (value.Contains("\""))
+                    {
+                        value = value.Replace('\"', '\'');
+                    }
+                    jsonString += '\"' + value + '\"';
+                }
+                else
+                {
+                    jsonString += ((string)item.Value.ToString());
+                }
+                jsonString += ',';
+            }
+            if (1 < jsonString.Length)
+            {
+                jsonString = jsonString.Remove(jsonString.Length - 1);
+            }
+            jsonString += "}";
+            Console.WriteLine("Json: " + jsonString);
+            return jsonString;
+        }
+
+        public static String toJsonStr(List<Dictionary<String, object>> dataList)
+        {
+            if (null == dataList) return null;
+            String jsonStr = "[";
+            foreach (Dictionary<String, object> item in dataList)
+            {
+                jsonStr += toJsonStr(item);
+                jsonStr += ",";
+            }
+            if (1 < jsonStr.Length)
+            {
+                jsonStr = jsonStr.Remove(jsonStr.Length - 1);
+            }
+            jsonStr += "]";
+            return jsonStr;
+        }
+
+    }
+}
---
+++ VirtualizationDB/SqlDepot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallerAppServer.VirtualizationDB
+{
+    class SqlDepot
+    {
+        private static int currentRecordId = 100;
+
+        public static string LoginUser()
+        {
+            return "SELECT * FROM EmployeeList WHERE CodeName=@CodeName AND Password=@Password And Active=TRUE";
+        }
+
+
+        public static String FindRecord()
+        {
+            return "SELECT Files.FileNumber, InsuredList.Name, Customers.Name, EmployeeList.Name, Files.SuitNumber, FileStatus.Name, Files.CreationDate " + 
+                "FROM((((Files LEFT JOIN FileCustomerLink ON Files.FilesId = FileCustomerLink.FileId) LEFT JOIN Customers ON FileCustomerLink.CustomerId = Customers.CustomersId) " +
+                "LEFT JOIN InsuredList ON Files.InsuredId = InsuredList.InsuredListId) LEFT JOIN EmployeeList ON Files.MainApprasierId = EmployeeList.EmployeeListId) " +
+                "LEFT JOIN FileStatus ON Files.FileStatus = FileStatus.FileStatusId " +
+                "WHERE Files.FileNumber = @FileNumber";
+        }
+
+        public static void SaveRecord(List<KeyValuePair<string, string>> recData)
+        {
+            // TODO
+        }
+
+        public virtual string getNextRecordId()
+        {
+            return currentRecordId++.ToString(); // TODO
+        }
+    }
+}
