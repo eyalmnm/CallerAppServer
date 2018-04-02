b61e7a61e8dffe190a75bdb28e25a3ec8ad656f2
eyalmnm <eyal@em-projects.com>
2018-04-02 11:41:32 +0300

    Add dependency in web.config

--- Utils/OtpUtils.cs
+++ Utils/OtpUtils.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+// Ref: https://stackoverflow.com/questions/22208413/generate-random-6-digit-number
+
 namespace CallerAppServer.Utils
 {
     public class OtpUtils
